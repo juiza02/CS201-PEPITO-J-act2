28b9cabcafae9eaedb4cad4fba8dc9bb597ebade
juiza02 <145626082+juiza02@users.noreply.github.com>
2023-10-20 14:02:15 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,39 @@
+let firstFavNum=14, secondFavNum=2;
+
+
+firstFavNum > secondFavNum
+
+  console.log("firstFavNum is greater than secondFavnum");
+  
+ console.log("firstFavNum is less than secondFavnum");
+
+  console.log("firstFavNum is greater than equal to secondFavnum");
+
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+  
+   firstFavNum = secondFavNum
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+
+  firstFavNum == secondFavNum
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+
+   firstFavNum !== secondFavNum
+
+
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+  
+  
+  
+  console.log("firstFavNum is equal to secondFavnum");
